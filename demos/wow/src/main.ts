2b862d3d9cf4117f46edea3c46a466ac84cf146d
huhaonan <huhaonan@sics.ac.cn>
2023-06-20 18:32:43 +0800

    feat: wowjs + anmate.css 简单例子

--- demos/wow/src/main.ts
+++ demos/wow/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'animate.css'
+import 'wow.js/css/libs/animate.css'
+import '@/styles/global.css'
 
 async function bootstrap() {
   const app = createApp(App)
