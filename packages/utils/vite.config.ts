ec0e566776a88d92a0ba8e927c18bee81b44f76a
huhaonan <huhaonan@sics.ac.cn>
2023-07-29 14:25:28 +0800

    build(util): utils 构建命令·

--- packages/utils/vite.config.ts
+++ packages/utils/vite.config.ts
@@ -2,16 +2,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    // 产物输出目录。
-    // outDir: 'dist',
+    // outDir: 'dist', // 产物输出目录默认 dist
 
     lib: {
       // 构建的入口文件
       entry: './index.ts',
-
-      // 产物的生成格式，默认为 ['es', 'umd']。我们使用默认值，注释掉此字段。
-      // formats: ['es', 'umd'],
-
       // 当产物为 umd、iife 格式时，该模块暴露的全局变量名称
       name: 'CustomUtil',
       // 产物文件名称
