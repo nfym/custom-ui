c621819401b8cd03047261ffb73aa7d7b1d1b689
huhaonan <huhaonan@sics.ac.cn>
2023-08-17 12:36:38 +0800

    types: 调整tsconfig

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <h1>随便看看</h1>
   <UiButton>btn</UiButton>
+  <TestComp></TestComp>
 </template>
 
 <script setup lang="ts">
 import { test } from '@custom-ui/utils'
 import { UiButton } from '@custom-ui/components'
+import TestComp from './components/TestComp.vue'
+// import TestComp from '@/components/TestComp.vue'
 
 test()
 </script>
