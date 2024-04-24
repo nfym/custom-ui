81b5da52e92297edb8e990108b24bfdb53a890ea
huhaonan <huhaonan@sics.ac.cn>
2024-04-24 14:50:16 +0800

    feat:完善工具方法注释

--- packages/utils/src/is/isType.ts
+++ packages/utils/src/is/isType.ts
@@ -2,6 +2,9 @@
  * 是否为指定的数据类型
  * @param value unknown
  * @param type string
+ * @demo isType(3, 'Number') => true
+ * @demo isType([1, 2, 3],'Array') => true
+ * @demo isType('abc','String') => true
  * @return boolean
  */
 export default function isType(value: unknown, type: string) {
