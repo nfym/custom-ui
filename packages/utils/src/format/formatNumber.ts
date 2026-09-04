c0413c6377208c4238ba8e635348a2f17a5c4ef6
huhaonan <huhaonan@sics.ac.cn>
2026-09-04 11:39:54 +0800

    style: 格式化

--- packages/utils/src/format/formatNumber.ts
+++ packages/utils/src/format/formatNumber.ts
@@ -1,8 +1,8 @@
-/** 
+/**
  * @Description: 数据展示处理
  */
 
-type Data = number | string | null;
+type Data = number | string | null
 
 /**
  * @description: 数据展示处理, 保留小数位
@@ -12,8 +12,8 @@ type Data = number | string | null;
 export const setFloat = (data: Data, num = 4) => {
   return data || data === 0
     ? +(Math.round(+data * Math.pow(10, num)) / Math.pow(10, num)).toFixed(num)
-    : data;
-};
+    : data
+}
 
 /**
  * @description: 数据展示处理, 格式化为百分比
@@ -22,9 +22,9 @@ export const setFloat = (data: Data, num = 4) => {
  * @param mini 为 true 时处理小数据显示成 <0.01%
  */
 export const setPercentage = (data: Data, num = 2, mini = false) => {
-  if (!data) return "";
-  if (+(data || 0) === 0) return "0%";
-  if (+(data || 0) < 0.0001 && mini) return "<0.01%";
-  
-  return Math.round(+data * Math.pow(10, num + 2)) / Math.pow(10, num) + "%";
-};
+  if (!data) return ''
+  if (+(data || 0) === 0) return '0%'
+  if (+(data || 0) < 0.0001 && mini) return '<0.01%'
+
+  return Math.round(+data * Math.pow(10, num + 2)) / Math.pow(10, num) + '%'
+}
